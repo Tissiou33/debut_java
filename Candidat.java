8f9d868b42e83b76dc844050db09d3a47d3dc70e
TOSSIM salomon <salomontossim3@gmail.com>
2023-11-19 09:50:12 +0000

    debut_programmation java

---
+++ Candidat.java
@@ -0,0 +1,7 @@
+public class Candidat
+{
+    public Candidat()
+    {
+        system.out.println("Je suis le candidat :" + this);
+    }
+}
---
+++ Fichier.java
@@ -0,0 +1,9 @@
+public class Fichier{
+    public static void main( strings []args)
+    {
+        system.out.println("Hello");
+
+        Candidat A = new Candidat();
+        Candidat B =new Candidat();
+    }
+}
