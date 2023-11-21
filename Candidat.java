3cd55fe43892784f319e35f89dbf053d401bc4c7
TOSSIM <141507471+Tissiou33@users.noreply.github.com>
2023-11-21 19:16:41 +0000

    Update Candidat.java

--- Candidat.java
+++ Candidat.java
@@ -2,6 +2,6 @@ public class Candidat
 {
     public Candidat()
     {
-        system.out.println("Je suis le candidat :" + this);
+        System.out.println("Je suis le candidat :" + this);
     }
 }
